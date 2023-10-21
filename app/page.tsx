46ef48fbaab7a23251de110b18bb61e75559e536
coderx0 <sumeetdeb100@gmail.com>
2023-10-21 21:31:06 +0530

    build error fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import ProjectFeed from '@/components/projectFeed';
-import { Project } from '@prisma/client';
 import axios from 'axios'
 import React from 'react'
+
+
+export const dynamic = 'force-dynamic'
+export const fetchCache = 'force-no-store'
+
 
 const page = async() => {
   const {data} = await axios.get(`${process.env.SITE_URL}/api/getProjects`);
   
   return (
     <>
-    {/* <h1>hello</h1> */}
       <ProjectFeed data={data}/>
     </>
   )
